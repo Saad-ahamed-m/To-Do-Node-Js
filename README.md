8559ada7653c8ca6fb817a36b6ef97676d4d34a6
Saad Ahamed M <saad.ahamed.m@gmail.com>
2020-08-31 12:18:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# To-Do-Node-Js
+This is a simple To-Do application developed using nodejs , express and mongodb.
+
+## Prerequisites
+Install the newest Nodejs version on your machine.
+
+## Getting Started
+
+# Create a MongoDB Cloud Instance
+[Get the connection_string](https://www.mongodb.com)
+
+Copy the CONNECTION_STRING value to `.env` file
+
+```
+npm install 
+npm run dev
+```
+
+# Screenshots of the Application
+
+# Application UI
+
+<img src="/Screenshots/1%20Application.png">
+
+Create Operation
+
+<img src="/Screenshots/2%20Create.png">
+
+# Read Operaration
+
+<img src="/Screenshots/3%20Read.png">
+
+# Update Operation
+
+<img src="/Screenshots/4%20Update.png">
+
+# Delete Operation
+
+<img src="/Screenshots/5%20Delete.png">
+
+
+
+                                                                  Happy Coding ! ✌
---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config();
+const app = express()
+
+const connectionString = process.env.CONNECTION_STRING;
+const mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
+const TodoTask = require("./models/TodoTask.js");
+const { ObjectID } = require('mongodb');
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/static', express.static("public"))
+const MongoClient = require("mongodb").MongoClient;
+app.listen(3000, () => { console.log('App is running in port 3000!! ') })
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then((client) => {
+        const db = client.db("To-Do-Application");
+        const simple_messages = db.collection("To_Do_List");
+
+        app.get("/", (req, res) => {
+            simple_messages
+                .find()
+                .toArray()
+                .then((results) => {
+                    res.render("todo.ejs", { content: results });
+                })
+                .catch((error) => console.error(error));
+        });
+        app.post("/", (req, res) => {
+            simple_messages
+                .insertOne(req.body)
+                .then((result) => {
+                    res.redirect("/");
+                })
+                .catch((error) => console.error(error));
+        });
+        app
+            .route("/edit/:id")
+            .get((req, res) => {
+                const id = req.params.id;
+                TodoTask.find({}, (err, tasks) => {
+                    res.render("todoEdit.ejs", { content: tasks, idTask: id });
+                });
+            })
+            .post((req, res) => {
+                const id_val = req.params.id;
+                const queryvalue = { _id: ObjectID(id_val) };
+                const new_val = { $set: { content: req.body.content } };
+                simple_messages.updateOne(queryvalue, new_val, function(error, res) {})
+                res.redirect("/")
+            });
+        app.route("/remove/:id").get((req, res) => {
+            const id_val = req.params.id;
+            const queryvalue = { _id: ObjectID(id_val) };
+            simple_messages.remove(queryvalue, function(err, obj) {
+                if (err) throw err;
+                else
+                    res.redirect("/");
+            });
+
+        });
+    })
+    .catch((error) => console.error(error));
