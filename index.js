9f2ad006d1cca9df027b8fca27964ea4cba3f4ab
Saad Ahamed M <saad.ahamed.m@gmail.com>
2020-09-01 10:47:01 +0530

    Few Updates were made

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config();
+var JSAlert = require("js-alert");
 const app = express()
-
 const connectionString = process.env.CONNECTION_STRING;
 const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
@@ -16,9 +16,18 @@ app.listen(3000, () => { console.log('App is running in port 3000!! ') })
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then((client) => {
         const db = client.db("To-Do-Application");
-        const simple_messages = db.collection("To_Do_List");
-
+        var simple_messages;
+        var passcode;
         app.get("/", (req, res) => {
+            res.render("todoChoice.ejs");
+        });
+        app.get("/login", (req, res) => {
+            res.render("todoLogin.ejs")
+        });
+        app.get("/register", (req, res) => {
+            res.render("todoRegister.ejs")
+        });
+        app.get("/signup_account", (req, res) => {
             simple_messages
                 .find()
                 .toArray()
@@ -26,37 +35,95 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
                     res.render("todo.ejs", { content: results });
                 })
                 .catch((error) => console.error(error));
-        });
+        })
+        app.post("/signup_account", (req, res) => {
+            passcode = req.body.name + req.body.password;
+            db.listCollections({ name: passcode })
+                .next(function(err, collinfo) {
+                    if (collinfo) {
+                        res.render("todoRegister-alert");
+                    } else {
+                        simple_messages = db.collection(passcode);
+                        simple_messages
+                            .find()
+                            .toArray()
+                            .then((results) => {
+                                res.render("todo.ejs", { content: results });
+                            })
+                            .catch((error) => console.error(error));
+                    }
+
+                });
+        })
+        app.post("/login_account", (req, res) => {
+            passcode = req.body.name + req.body.password;
+            db.listCollections({ name: passcode })
+                .next(function(err, collinfo) {
+                    if (!collinfo) {
+                        res.render("todoLogin-alert")
+                    } else {
+                        simple_messages = db.collection(passcode);
+                        simple_messages
+                            .find()
+                            .toArray()
+                            .then((results) => {
+                                res.render("todo.ejs", { content: results });
+                            })
+                            .catch((error) => console.error(error));
+                    }
+
+                });
+        })
         app.post("/", (req, res) => {
+            simple_messages = simple_messages = db.collection(passcode);
             simple_messages
                 .insertOne(req.body)
                 .then((result) => {
-                    res.redirect("/");
+                    simple_messages
+                        .find()
+                        .toArray()
+                        .then((results) => {
+                            res.render("todo.ejs", { content: results });
+                        })
+                        .catch((error) => console.error(error));
                 })
-                .catch((error) => console.error(error));
+                .catch((error) => console.log(error));
         });
         app
             .route("/edit/:id")
             .get((req, res) => {
                 const id = req.params.id;
-                TodoTask.find({}, (err, tasks) => {
-                    res.render("todoEdit.ejs", { content: tasks, idTask: id });
-                });
+                res.render("todoEdit.ejs", { idTask: id });
             })
             .post((req, res) => {
                 const id_val = req.params.id;
                 const queryvalue = { _id: ObjectID(id_val) };
                 const new_val = { $set: { content: req.body.content } };
                 simple_messages.updateOne(queryvalue, new_val, function(error, res) {})
-                res.redirect("/")
+                simple_messages
+                    .find()
+                    .toArray()
+                    .then((results) => {
+                        res.render("todo.ejs", { content: results });
+                    })
+                    .catch((error) => console.error(error));
+
             });
         app.route("/remove/:id").get((req, res) => {
+            simple_messages = simple_messages = db.collection(passcode);
             const id_val = req.params.id;
             const queryvalue = { _id: ObjectID(id_val) };
             simple_messages.remove(queryvalue, function(err, obj) {
                 if (err) throw err;
-                else
-                    res.redirect("/");
+                else {
+                    simple_messages
+                        .find()
+                        .toArray()
+                        .then((results) => {
+                            res.render("todo.ejs", { content: results });
+                        })
+                        .catch((error) => console.error(error));
+                }
             });
 
         });
